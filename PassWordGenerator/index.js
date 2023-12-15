5d77d8c6188271bec83c3ea86d4eeba847b81959
Saloni Lathwariya <125601277+Saloni581@users.noreply.github.com>
2023-12-15 14:08:36 +0530

    Add files via upload
    Created a random password generetor using vanilla javascript.

---
+++ PassWordGenerator/index.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passWord1El = document.getElementById("psw1-el");
+let passWord2El = document.getElementById("psw2-el");
+
+function getPasswords() {
+  for (let i = 0; i < 8; i++) {
+    passWord1El.textContent +=
+      characters[Math.floor(Math.random() * characters.length)];
+  }
+  for (let i = 0; i < 8; i++) {
+    passWord2El.textContent +=
+      characters[Math.floor(Math.random() * characters.length)];
+  }
+}
+
+function generatePasswords() {
+  passWord1El.textContent = "";
+  passWord2El.textContent = "";
+  getPasswords();
+}
